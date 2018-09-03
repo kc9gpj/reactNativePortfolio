842b8e54694335b140851e0ed78fddb8cce2cd3f
David Hoffmann <kc9gpj12@gmail.com>
2018-09-03 14:46:04 -0500

    none

--- App.js
+++ App.js
@@ -2,35 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Linking, Button,} from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 
-
-export default createBottomTabNavigator({
-  Home: HomeScreen,
-  Portfolio: PortfolioScreen,
-});
-
-
-
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.containerHome}>
         <Text style={styles.bigRed}>David Hoffmann</Text>
-        <Text style={{color: '#ba2f30'}}
-      onPress={() => Linking.openURL('https://www.davidhoffmann.me/')}>
-                Portfolio Website
-      </Text>        
-      <Text style={{color: '#ba2f30'}}>david.p.hoffmann1@gmail.com</Text>
+        <Button
+      onPress={() => Linking.openURL('https://www.davidhoffmann.me/')}
+      title="Portfolio Website"/>
+                
+           
+      <Button onPress={() => Linking.openURL('mailto:david.p.hoffmann1@gmail.com') }
+      title="Email" />      
       <Text></Text>
         <Image
           style={{width: 256, height: 341, borderRadius: 15}}
           source={{uri: 'https://i.imgur.com/hY6t62p.jpg'}}
         />
-        <Button
-        title="Go to Dave's Portfolio"
-        onPress={() =>
-          navigate('Portfolio')
-        }
-      />
+    
       </View>
     );
   }
@@ -40,23 +29,95 @@ class PortfolioScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.bigRed}>David Hoffmann</Text>
-      
-        <Button
-        title="Home"
-        onPress={() =>
-          navigate('Home')
-        }
-      />
+        <Text style={styles.bigRed}>Portfolio</Text>
+        <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/jwR64t5.png'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/Hangman')}>
+                Hangman 
+      </Text>  
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/CGTog6U.jpg'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/StarWarsRPG')}>
+                Star Wars 
+      </Text>  
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/VcUbtvC.jpg'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/TriviaGame')}>
+                Trivia 
+      </Text>  
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/o1ef1LF.jpg'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/GifTastic')}>
+                Giftastic
+      </Text>
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/KSYWvtv.png'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/TrainSchedule')}>
+                Train Scheduler
+      </Text>
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/JsGa604.png'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/FoodSearch')}>
+                Food Search
+      </Text>
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/wYSi9xN.png'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/dogfriendfinder')}>
+                Dog Friend Finder
+      </Text>
+      <Image
+          style={{width: 50, height: 50, borderRadius: 15}}
+          source={{uri: 'https://i.imgur.com/EmUhCtn.jpg'}}
+        />
+    <Text style={{color: '#ba2f30'}}
+      onPress={() => Linking.openURL('https://github.com/kc9gpj/burger')}>
+                Eat-Da-Burger
+      </Text>
+    
+    
       </View>
     );
   }
 }
 
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Portfolio: PortfolioScreen,
+});
+
 const styles = StyleSheet.create({
+  containerHome: {
+    flex: 1,
+    backgroundColor: '#12aeac',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
   container: {
     flex: 1,
-    backgroundColor: '#0d8989',
+    flexDirection: 'column',
+    backgroundColor: '#12aeac',
     alignItems: 'center',
     justifyContent: 'center',
 
@@ -75,4 +136,5 @@ const styles = StyleSheet.create({
     margin: 20,
     backgroundColor: 'white',
   },
+  
 });
